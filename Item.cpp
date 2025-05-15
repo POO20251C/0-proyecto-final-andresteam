bf093dfa2cb78c13e899c5cecc277466673ff7ec
Andres Diaz <andresfeliped078@gmail.com>
2025-05-15 17:08:04 -0500

    Se crea la clase de implementacion de Item

--- Item.cpp
+++ Item.cpp
@@ -1,3 +1,16 @@
-//
-// Created by andre on 13/05/2025.
-//
+#include "Item.h"
+#include <iostream>
+
+using namespace std;
+
+Item::Item(string nombre, string tipo, int atk, int def, int spd, int lck, int cura)
+    : nombre(nombre), tipo(tipo), modATK(atk), modDEF(def), modSPD(spd), modLCK(lck), curacion(cura) {}
+
+void Item::mostrar() const {
+    cout << "Item: " << nombre << " | Tipo: " << tipo
+              << " | ATK: +" << modATK
+              << " | DEF: +" << modDEF
+              << " | SPD: +" << modSPD
+              << " | LCK: +" << modLCK
+              << " | Cura: +" << curacion << endl;
+}
