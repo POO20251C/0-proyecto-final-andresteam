4d9ec55935c4bf4c0ad4a42b27721af49132c3c7
Andres Diaz <andresfeliped078@gmail.com>
2025-05-15 17:06:34 -0500

    Se crea la clase Enemigo

--- Enemigo.h
+++ Enemigo.h
@@ -1,8 +1,16 @@
-//
-// Created by andre on 15/05/2025.
-//
-
 #ifndef ENEMIGO_H
 #define ENEMIGO_H
+
+#include <string>
+#include "Personaje.h"
+
+class Enemigo : public Personaje {
+private:
+    std::string tipo; // "Soldado", "MiniJefe", "GranJefe"
+
+public:
+    Enemigo(std::string nombre, std::string tipo, int hp, int atk, int def, int spd, int lck);
+    std::string getTipo() const;
+};
 
 #endif //ENEMIGO_H
