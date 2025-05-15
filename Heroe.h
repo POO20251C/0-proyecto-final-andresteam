1a954f1ebb5ddf86f05667b29e9ce1bee61fd0e7
Andres Diaz <andresfeliped078@gmail.com>
2025-05-15 17:05:13 -0500

    Se crea la clase de Heroe

--- Heroe.h
+++ Heroe.h
@@ -1,8 +1,21 @@
-//
-// Created by andre on 13/05/2025.
-//
-
 #ifndef HEROE_H
 #define HEROE_H
+
+#include <vector>
+#include <string>
+#include "Personaje.h"
+#include "Item.h"
+
+
+class Heroe : public Personaje {
+private:
+    std::vector<Item> inventario;
+
+public:
+    Heroe(std::string nombre, int hp, int atk, int def, int spd, int lck);
+    void agregarItem(const Item& item);
+    void usarPocion();
+    void mostrarInventario() const;
+};
 
 #endif //HEROE_H
