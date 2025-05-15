8b9436f5f3982cced6d1450621dfcabd6d43ba32
Andres Diaz <andresfeliped078@gmail.com>
2025-05-15 17:06:05 -0500

    Se crea la clase de implementacion del Heroe

--- Heroe.cpp
+++ Heroe.cpp
@@ -1,3 +1,31 @@
-//
-// Created by andre on 13/05/2025.
-//
+#include <iostream>
+#include "Heroe.h"
+
+using namespace std;
+
+Heroe::Heroe(string nombre, int hp, int atk, int def, int spd, int lck)
+    : Personaje(nombre, hp, atk, def, spd, lck) {}
+
+void Heroe::agregarItem(const Item& item) {
+    inventario.push_back(item);
+    cout << nombre << " obtuvo el item: ";
+    item.mostrar();
+}
+
+void Heroe::usarPocion() {
+    for (const auto& item : inventario) {
+        if (item.getTipo() == "pocion") {
+            hp += item.getCuracion();
+            cout << nombre << " usó una poción. Nueva HP: " << hp << endl;
+            return;
+        }
+    }
+    cout << nombre << " no tiene pociones." << endl;
+}
+
+void Heroe::mostrarInventario() const {
+    cout << "Inventario de " << nombre << ":\n";
+    for (const auto& item : inventario) {
+        item.mostrar();
+    }
+}
