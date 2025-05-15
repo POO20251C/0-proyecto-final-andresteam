eefc0160df7c8b81bf8b5a161dc1265fe1d4dd3b
Andres Diaz <andresfeliped078@gmail.com>
2025-05-15 17:07:42 -0500

    Se crea la clase de Item, por ahora solo la base de la clase

--- Item.h
+++ Item.h
@@ -1,8 +1,20 @@
-//
-// Created by andre on 13/05/2025.
-//
-
 #ifndef ITEM_H
 #define ITEM_H
+
+#include <string>
+
+class Item {
+private:
+    std::string nombre;
+    std::string tipo;
+    int modATK, modDEF, modSPD, modLCK, curacion;
+
+public:
+    Item(std::string nombre, std::string tipo, int atk = 0, int def = 0, int spd = 0, int lck = 0, int cura = 0);
+    void mostrar() const;
+
+    std::string getTipo() const { return tipo; }
+    int getCuracion() const { return curacion; }
+};
 
 #endif //ITEM_H
