2adb0673e9ec7826bb7556f73c213b12080875fb
Andres Diaz <andresfeliped078@gmail.com>
2025-05-15 17:04:28 -0500

    Se crea la clase de implementacion del Personaje

--- Personaje.cpp
+++ Personaje.cpp
@@ -1,3 +1,28 @@
-//
-// Created by andre on 13/05/2025.
-//
+#include <string>
+#include <iostream>
+#include "Personaje.h"
+
+using namespace std;
+
+Personaje::Personaje(string nombre, int hp, int atk, int def, int spd, int lck)
+    : nombre(nombre), hp(hp), atk(atk), def(def), spd(spd), lck(lck) {}
+
+void Personaje::recibirDano(int cantidad) {
+    int danoFinal = cantidad - def;
+    if (danoFinal < 0) danoFinal = 0;
+    hp -= danoFinal;
+    cout << nombre << " recibe " << danoFinal << " de daño. HP restante: " << hp << endl;
+}
+
+bool Personaje::estaVivo() const {
+    return hp > 0;
+}
+
+void Personaje::mostrarStats() const {
+    cout << "Nombre: " << nombre
+              << " | HP: " << hp
+              << " | ATK: " << atk
+              << " | DEF: " << def
+              << " | SPD: " << spd
+              << " | LCK: " << lck << endl;
+}
