4110d741ee2c7f9f9f4e83770e499143f6501e19
Andres Diaz <andresfeliped078@gmail.com>
2025-05-21 10:18:44 -0500

    Se modifica la clase de Item la cual se manejara los diferentes items del inventario del jugador

--- Item.cpp
+++ Item.cpp
@@ -1,16 +1,24 @@
 #include "Item.h"
 #include <iostream>
-
 using namespace std;
 
-Item::Item(string nombre, string tipo, int atk, int def, int spd, int lck, int cura)
-    : nombre(nombre), tipo(tipo), modATK(atk), modDEF(def), modSPD(spd), modLCK(lck), curacion(cura) {}
+Item::Item(string nombre, string tipo, int modATK, int modDEF, int modSPD, int modLCK, int curacion)
+    : nombre(nombre), tipo(tipo), modATK(modATK), modDEF(modDEF), modSPD(modSPD), modLCK(modLCK), curacion(curacion) {}
 
 void Item::mostrar() const {
-    cout << "Item: " << nombre << " | Tipo: " << tipo
-              << " | ATK: +" << modATK
-              << " | DEF: +" << modDEF
-              << " | SPD: +" << modSPD
-              << " | LCK: +" << modLCK
-              << " | Cura: +" << curacion << endl;
+    cout << "Item: " << nombre << " | Tipo: " << tipo;
+    if (modATK) cout << " | +ATK: " << modATK;
+    if (modDEF) cout << " | +DEF: " << modDEF;
+    if (modSPD) cout << " | +SPD: " << modSPD;
+    if (modLCK) cout << " | +LCK: " << modLCK;
+    if (curacion) cout << " | Cura: +" << curacion;
+    cout << endl;
 }
+
+string Item::getNombre() const { return nombre; }
+string Item::getTipo() const { return tipo; }
+int Item::getModATK() const { return modATK; }
+int Item::getModDEF() const { return modDEF; }
+int Item::getModSPD() const { return modSPD; }
+int Item::getModLCK() const { return modLCK; }
+int Item::getCuracion() const { return curacion; }
--- Item.h
+++ Item.h
@@ -6,15 +6,26 @@
 class Item {
 private:
     std::string nombre;
-    std::string tipo;
-    int modATK, modDEF, modSPD, modLCK, curacion;
+    std::string tipo; // "arma", "armadura", "pocion", "accesorio"
+    int modATK;
+    int modDEF;
+    int modSPD;
+    int modLCK;
+    int curacion; // Si es una poción, cuánta vida recupera
 
 public:
-    Item(std::string nombre, std::string tipo, int atk = 0, int def = 0, int spd = 0, int lck = 0, int cura = 0);
+    Item(std::string nombre, std::string tipo,
+         int modATK = 0, int modDEF = 0, int modSPD = 0, int modLCK = 0, int curacion = 0);
+
     void mostrar() const;
 
-    std::string getTipo() const { return tipo; }
-    int getCuracion() const { return curacion; }
+    std::string getNombre() const;
+    std::string getTipo() const;
+    int getModATK() const;
+    int getModDEF() const;
+    int getModSPD() const;
+    int getModLCK() const;
+    int getCuracion() const;
 };
 
 #endif //ITEM_H
