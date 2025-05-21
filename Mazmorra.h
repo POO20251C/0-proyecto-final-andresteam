4113772415ae417633a86374ca73096f5bc8d6a3
Andres Diaz <andresfeliped078@gmail.com>
2025-05-21 09:46:12 -0500

    Se crea la clase de mazmorra

--- Mazmorra.h
+++ Mazmorra.h
@@ -1,8 +1,19 @@
-//
-// Created by andre on 20/05/2025.
-//
-
 #ifndef MAZMORRA_H
 #define MAZMORRA_H
+
+#include <vector>
+#include "Sala.h"
+#include "Heroe.h"
+#include "Enemigo.h"
+
+class Mazmorra {
+private:
+    std::vector<Sala> salas;
+
+public:
+    Mazmorra();
+    void iniciarRecorrido(std::vector<Heroe*>& heroes);
+    int obtenerSalaActual() const;
+};
 
 #endif //MAZMORRA_H
