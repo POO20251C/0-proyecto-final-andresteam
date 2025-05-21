d7fe08f22697c452451773e001d5d66f87a83cbf
Andres Diaz <andresfeliped078@gmail.com>
2025-05-21 10:17:35 -0500

    Se hacen modificaciones a la clasede Heroe y se maneja ya un menu de interaccion

--- Heroe.cpp
+++ Heroe.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include "Heroe.h"
-
 using namespace std;
 
 Heroe::Heroe(string nombre, int hp, int atk, int def, int spd, int lck)
@@ -8,24 +7,28 @@ Heroe::Heroe(string nombre, int hp, int atk, int def, int spd, int lck)
 
 void Heroe::agregarItem(const Item& item) {
     inventario.push_back(item);
-    cout << nombre << " obtuvo el item: ";
-    item.mostrar();
-}
-
-void Heroe::usarPocion() {
-    for (const auto& item : inventario) {
-        if (item.getTipo() == "pocion") {
-            hp += item.getCuracion();
-            cout << nombre << " usó una poción. Nueva HP: " << hp << endl;
-            return;
-        }
-    }
-    cout << nombre << " no tiene pociones." << endl;
+    cout << nombre << " ha obtenido el item: " << item.getNombre() << endl;
 }
 
 void Heroe::mostrarInventario() const {
     cout << "Inventario de " << nombre << ":\n";
+    if (inventario.empty()) {
+        cout << "  (Vacío)\n";
+        return;
+    }
     for (const auto& item : inventario) {
         item.mostrar();
     }
+}
+
+void Heroe::usarPocion() {
+    for (size_t i = 0; i < inventario.size(); ++i) {
+        if (inventario[i].getTipo() == "pocion") {
+            hp += inventario[i].getCuracion();
+            cout << nombre << " usó una poción. HP restaurado a " << hp << endl;
+            inventario.erase(inventario.begin() + i);
+            return;
+        }
+    }
+    cout << nombre << " no tiene pociones disponibles.\n";
 }
--- Heroe.h
+++ Heroe.h
@@ -6,16 +6,16 @@
 #include "Personaje.h"
 #include "Item.h"
 
-
 class Heroe : public Personaje {
 private:
     std::vector<Item> inventario;
 
 public:
     Heroe(std::string nombre, int hp, int atk, int def, int spd, int lck);
+
     void agregarItem(const Item& item);
-    void usarPocion();
     void mostrarInventario() const;
+    void usarPocion();
 };
 
 #endif //HEROE_H
