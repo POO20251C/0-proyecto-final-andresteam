cbe26ec505f1c0135ec29d4bea5f573b661a9c20
Andres Diaz <andresfeliped078@gmail.com>
2025-05-21 09:49:59 -0500

    Se crea la implementacion de la clase

--- Score.cpp
+++ Score.cpp
@@ -1,3 +1,24 @@
-//
-// Created by andre on 20/05/2025.
-//
+#include <sstream>
+#include "Score.h"
+using namespace std;
+
+Score::Score(string alias, string fechaHora, int salaMax, int saludPerdida)
+    : alias(alias), fechaHora(fechaHora), salaMax(salaMax), saludPerdida(saludPerdida) {}
+
+string Score::getAlias() const { return alias; }
+string Score::getFechaHora() const { return fechaHora; }
+int Score::getSalaMax() const { return salaMax; }
+int Score::getSaludPerdida() const { return saludPerdida; }
+
+string Score::toString() const {
+    stringstream ss;
+    ss << alias << " | " << fechaHora << " | Sala alcanzada: " << salaMax
+       << " | Salud perdida: " << saludPerdida;
+    return ss.str();
+}
+
+bool Score::operator<(const Score& other) const {
+    if (salaMax != other.salaMax)
+        return salaMax > other.salaMax; // Más salas es mejor
+    return saludPerdida < other.saludPerdida; // Menos daño es mejor
+}
