21acc1bc8b01d1a1b5c0607b5fcce688e8feec22
Andres Diaz <andresfeliped078@gmail.com>
2025-05-21 09:49:26 -0500

    Se crea la clase del Score

--- Score.h
+++ Score.h
@@ -1,8 +1,27 @@
-//
-// Created by andre on 20/05/2025.
-//
-
 #ifndef SCORE_H
 #define SCORE_H
+
+#include <string>
+
+class Score {
+private:
+    std::string alias;
+    std::string fechaHora;
+    int salaMax;
+    int saludPerdida;
+
+public:
+    Score(std::string alias, std::string fechaHora, int salaMax, int saludPerdida);
+
+    std::string getAlias() const;
+    std::string getFechaHora() const;
+    int getSalaMax() const;
+    int getSaludPerdida() const;
+
+    std::string toString() const;
+
+    // Para ordenar: primero por salaMax, luego por menor salud perdida
+    bool operator<(const Score& other) const;
+};
 
 #endif //SCORE_H
